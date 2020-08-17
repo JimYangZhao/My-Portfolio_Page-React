78289684c5dc89c44141ce8f8ab1a3a13168f5d8
JimYangZhao <yangzhao264@gmail.com>
2020-08-17 02:09:53 -0230

    Deploy Page

---
+++ build/precache-manifest.021c76780dcc28e5437112618235511b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1df6e3d50e3f854d3a71",
+    "url": "/JimYangZhao/My-Portfolio_Page-React/static/css/main.2ba997e0.chunk.css"
+  },
+  {
+    "revision": "1df6e3d50e3f854d3a71",
+    "url": "/JimYangZhao/My-Portfolio_Page-React/static/js/main.1df6e3d5.chunk.js"
+  },
+  {
+    "revision": "63fb5b41de279ed09cdd",
+    "url": "/JimYangZhao/My-Portfolio_Page-React/static/js/1.63fb5b41.chunk.js"
+  },
+  {
+    "revision": "4a1dcbc8fcd10b0e1195",
+    "url": "/JimYangZhao/My-Portfolio_Page-React/static/js/runtime~main.4a1dcbc8.js"
+  },
+  {
+    "revision": "3cc38811e4484874fd5f825a511ec2ec",
+    "url": "/JimYangZhao/My-Portfolio_Page-React/index.html"
+  }
+];
